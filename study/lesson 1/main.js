74bc86ba9369536e82d04c0775c1fca5777f20bd
HappySpitz <tatarinkristina2002@gmail.com>
2022-11-07 17:22:13 +0200

    study

---
+++ study/lesson 1/main.js
@@ -0,0 +1,178 @@
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//    Вивести кожну змінну за допомогою: console.log , alert, document.write
+/*let one = 'hello';
+console.log(one);
+alert(one);
+document.write('hello ');
+
+let two = 'owu';
+console.log(two);
+alert(two);
+document.write('owu ');
+
+let three = 'com';
+console.log(three);
+alert(three);
+document.write('com ');
+
+let four = 'ua';
+console.log(four);
+alert(four);
+document.write('ua ');
+
+let five = 1;
+console.log(five)
+alert(five);
+document.write('1 ');
+
+let six = 10;
+console.log(six);
+alert(six);
+document.write('10 ');
+
+let seven = -999;
+console.log(seven);
+alert(seven);
+document.write('-999 ');
+
+let eight = 123;
+console.log(eight);
+alert(eight);
+document.write('123 ');
+
+let nine = 3.14;
+console.log(nine);
+alert(nine);
+document.write('3.14 ');
+
+let ten = 2.7;
+console.log(ten);
+alert(ten);
+document.write('2.7 ');
+
+let eleven = 16;
+console.log(eleven);
+alert(eleven);
+document.write('16 ');
+
+let twelve = true;
+console.log(twelve);
+alert(twelve);
+document.write('true ');
+
+let thirteen = false;
+console.log(thirteen);
+alert(thirteen);
+document.write('false ');
+
+//- Переприсвоїти кожній змінній з завдання значення на довільне.
+one = 12;
+two = true;
+three = 'mother';
+four = 'father';
+five = 69;
+six = 2.25;
+seven = false;
+eight = 'grandma';
+nine = 'son';
+ten = 'aunt';
+eleven = 999;
+twelve = 'landlady';
+thirteen = 'love';
+//    Вивести кожну змінну за допомогою: console.log , alert, document.write
+console.log(one);
+alert(one);
+document.write(one + ' ');
+
+console.log(two);
+alert(two);
+document.write(two + ' ');
+
+console.log(three);
+alert(three);
+document.write(three + ' ');
+
+console.log(four);
+alert(four);
+document.write(four + ' ');
+
+console.log(five);
+alert(five);
+document.write(five + ' ');
+
+console.log(six);
+alert(six);
+document.write(six + ' ');
+
+console.log(seven);
+alert(seven);
+document.write(seven + ' ');
+
+console.log(eight);
+alert(eight);
+document.write(eight + ' ');
+
+console.log(nine);
+alert(nine);
+document.write(nine + ' ');
+
+console.log(ten);
+alert(ten);
+document.write(ten + ' ');
+
+console.log(eleven);
+alert(eleven);
+document.write(eleven + ' ');
+
+console.log(twelve);
+alert(twelve);
+document.write(twelve + ' ');
+
+console.log(thirteen);
+alert(thirteen);
+document.write(thirteen + ' ');*/
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//    Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+/*let hello = alert('Вітаю');
+let firstName = prompt('Твоє ім\'я?');
+let lastName = prompt('Твоє по-батькові?');
+let years = +prompt('Скільки тобі років?');
+console.log(hello,firstName,lastName,years)*/
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//    let a = 100; let b = '100'; let c = true;
+/*let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);*/
+
+//- Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//    В однакових виразах не використовувати однакові оператори!!!
+/*    5 ? 6 -> true
+5 ? 6 -> false
+5 ? 6 -> false
+5 ? 6 -> false
+10 ? 10 -> true
+10 ? 10 -> true
+10 ? 10 -> false
+10 ? 10 -> false
+10 ? 10 -> false
+123 ? '123' -> false
+123 ? '123' -> true*/
+let a = 5 < 6;
+console.log(a);
+
+let b = 5 > 6;
+console.log(b);
+
+let c = 5 === 6;
+console.log(c);
+
+let d = 5 >= 6;
+console.log(d);
